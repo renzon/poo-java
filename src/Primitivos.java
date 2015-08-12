77f13ae38f5f0949beb13b627ecc731dcf8643e5
renzon <renzon@gmail.com>
2015-08-12 08:59:07 -0300

    Primitivos - Boolean e Inteiro

---
+++ src/Primitivos.java
@@ -0,0 +1,39 @@
+
+public class Primitivos {
+
+	public static void main(String[] args) {
+		boolean b = true;
+		boolean a = false;
+		
+		if(a && b){
+			System.out.println("Executou");
+		}else if(a || b){
+			System.out.println("Else if");
+		}else{
+			System.out.println("Else");
+		}
+//		a="renzo";
+		
+		System.out.println("saiu do if");
+		
+		byte by = 127;
+		short s = 800;
+		int i=900;
+		long l=8000;
+		
+		s = by;
+		++s;
+		if (Byte.MIN_VALUE<=s && s<128){
+			by = (byte) s;
+		}
+		
+		System.out.println(Integer.MAX_VALUE);
+		
+		System.out.println(by);
+		
+		
+		
+
+	}
+
+}
